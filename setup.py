7e6fcab2239b721a5d9b73778c8d3f4e8ad3812b
JacopoMadaluni <jacopo.madaluni@gmail.com>
2023-04-07 20:32:28 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="python-barn",
-    version="0.2.1",
+    version="0.3.0",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
