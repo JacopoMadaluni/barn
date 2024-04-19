814f11d5887d6aaf417de8b85c1ab89d20baff1b
Jacopo Madaluni <32674729+JacopoMadaluni@users.noreply.github.com>
2024-04-19 12:07:39 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="python-barn",
-    version="0.10.0",
+    version="0.11.0",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
